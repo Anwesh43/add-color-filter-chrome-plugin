bd614a7d2d95e873942261fd6df1f980662fd2d2
Anwesh43 <anweshthecool0@gmail.com>
2017-08-01 23:12:35 +0530

    not filterifying images which are already filtered

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var funcCode = 'function applyFilter(img) {console.log("loaded");var image = new Image();image.crossOrigin = "Anonymous";image.src = img.src;console.log("coming here");image.onload = ()=>{var canvas = document.createElement("canvas");const imw = image.width,imh = image.height;canvas.width = image.width;canvas.height = image.height;var context = canvas.getContext("2d");context.drawImage(image,0,0);context.globalAlpha = 0.5;context.fillStyle = "orange";context.fillRect(0,0,imw,imh);try {img.src = canvas.toDataURL();}catch(exception){}}}'
+var k = 0
+var funcCode = 'function applyFilter(img) {if(!img.getAttribute("done")){var image = new Image();image.crossOrigin = "Anonymous";image.src = img.src;console.log("coming here");image.onload = ()=>{var canvas = document.createElement("canvas");const imw = image.width,imh = image.height;canvas.width = image.width;canvas.height = image.height;var context = canvas.getContext("2d");context.drawImage(image,0,0);context.globalAlpha = 0.5;context.fillStyle = "orange";context.fillRect(0,0,imw,imh);try {img.src = canvas.toDataURL();img.setAttribute("done",true);}catch(exception){}}}}'
 
 var code = 'var imgs = document.querySelectorAll("img");for(var i=0;i<imgs.length;i++){applyFilter(imgs[i])}'
 chrome.tabs.getCurrent((tab)=>{
