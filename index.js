04705cb5ee0fedadb5a0832d2624330731815f11
Anwesh43 <anweshthecool0@gmail.com>
2017-08-01 22:57:23 +0530

    adding minor fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var funcCode = 'function applyFilter(img) {console.log("loaded");var image = new Image();image.crossOrigin = "Anonymous";image.src = img.src;console.log("coming here");image.onload = ()=>{var canvas = document.createElement("canvas");const imw = image.width,imh = image.height;canvas.width = image.width;canvas.height = image.height;var context = canvas.getContext("2d");context.drawImage(image,0,0);context.globalAlpha = 0.4;context.fillStyle = "orange";context.fillRect(0,0,imw,imh);console.log(canvas);try {img.src = canvas.toDataURL();console.log(canvas.toDataURL())}catch(exception){}}}'
+var funcCode = 'function applyFilter(img) {console.log("loaded");var image = new Image();image.crossOrigin = "Anonymous";image.src = img.src;console.log("coming here");image.onload = ()=>{var canvas = document.createElement("canvas");const imw = image.width,imh = image.height;canvas.width = image.width;canvas.height = image.height;var context = canvas.getContext("2d");context.drawImage(image,0,0);context.globalAlpha = 0.5;context.fillStyle = "orange";context.fillRect(0,0,imw,imh);try {img.src = canvas.toDataURL();}catch(exception){}}}'
 
-var code = 'var imgs = document.querySelectorAll("img");console.log(imgs);for(var i=0;i<imgs.length;i++){applyFilter(imgs[i])}'
+var code = 'var imgs = document.querySelectorAll("img");for(var i=0;i<imgs.length;i++){applyFilter(imgs[i])}'
 chrome.tabs.getCurrent((tab)=>{
     console.log("getting current tab")
 })
